42123f7186049eea58a4cbe4773e02fcd39fd285
David Zonana <davidzn2@gmail.com>
2022-09-20 17:16:29 -0500

    Inital commit

---
+++ src/Componente.js
@@ -0,0 +1,11 @@
+
+function Componente(){
+    return(
+        <div>
+            <h2>Soy un componente</h2>
+            <h3>Y si, me importan desde otro lado jeje</h3>
+        </div>
+    )
+}
+
+export default Componente;
